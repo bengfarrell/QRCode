580c83f04fdef2cb560a5356f378b100fd1f84a9
Peter Braden <peterbraden@peterbraden.co.uk>
2013-07-14 22:31:23 -0700

    Start work on an interface

---
+++ index.js
@@ -0,0 +1,32 @@
+var grid = require('./src/grid')
+  , version = require('./src/version')
+  , detector = require('./src/detector')
+  , formatinf = require('./src/formatinf')
+  , errorlevel = require('./src/formatinf')
+  , bitmat = require('./src/formatinf')
+  , datablock
+  , bmparser
+  , datamask
+  , rsdecoder
+  , gf256poly
+  , gf256
+  , decoder
+  , qrcode
+  , findpat
+  , alignpat
+  , databr
+
+
+module.exports = {
+  encode : function(data, cb){
+
+  }
+
+, decode : function(im, cb){
+
+  }
+
+, detect : function(im cb){
+
+  }
+}
